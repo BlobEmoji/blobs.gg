1f3adca2139be4a71f5a2b6258e3f48c0c78239a
slice <ryaneft@gmail.com>
2020-05-03 12:43:56 -0700

    Add infinite scrolling in the community servers list (#21)

--- scripts/components/Servers.js
+++ scripts/components/Servers.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 
 import Server from './Server'
 import { shuffleArray } from '../utils'
+import { useScrollNearEnd } from '../hooks'
 
-const SERVERS_PER_PAGE = 6
+const SERVERS_PER_PAGE = 9
 
 export function Servers({ servers, ...props }) {
   return Object.entries(servers).map(([id, server]) => (
@@ -26,24 +27,11 @@ export function CommunityServers(props) {
     .slice(0, upTo)
     .map(([id, server]) => <Server key={id} server={{ id, ...server }} />)
 
-  function handleViewMoreClick() {
-    setUpTo((prev) => Math.min(prev + SERVERS_PER_PAGE, servers.length))
-  }
+  useScrollNearEnd(() => {
+    if (upTo < servers.length) {
+      setUpTo((prev) => Math.min(prev + SERVERS_PER_PAGE, servers.length))
+    }
+  })
 
-  return (
-    <>
-      <div className="servers community-servers">{serverNodes}</div>
-      {upTo < servers.length && (
-        <div className="view-more">
-          <button
-            type="button"
-            className="button"
-            onClick={handleViewMoreClick}
-          >
-            View More
-          </button>
-        </div>
-      )}
-    </>
-  )
+  return <div className="servers community-servers">{serverNodes}</div>
 }
