4bf91ab481c4a2d21adc4970dfea167546977fcc
Sam <Lagicrus@users.noreply.github.com>
2020-05-21 14:20:30 +0100

    Move to async and self contained components

--- scripts/changes.js
+++ scripts/changes.js
@@ -1,48 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import RecentChanges from './components/RecentChanges'
 
-import { log } from './utils'
-import RecentChanges from './components/RecentChanges';
-
-const HISTORY_ENDPOINT =
-  window.location.host.endsWith('now.sh') ||
-    'https://api.mousey.app/v3/emoji/blobs+community-blobs/changes';
-
-class Changes {
-  constructor(data) {
-
-    this.changes = {}
-
-    for (const change of data) {
-      if (['EMOJI_CREATE', 'EMOJI_RENAME', 'EMOJI_UPDATE', 'EMOJI_REMOVE'].includes(change.event)) {
-        const date = change.changed_at = new Date(change.changed_at);
-        const key = [change.guild.id, date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()];
-        this.changes[key] = (this.changes[key] ?? [])
-        this.changes[key].push(change);
-
-      }
-
-    }
-  }
-}
-
-
-function mount_history(data) {
-  const changes = new Changes(data);
-
-  log('changes:', changes)
-
-  ReactDOM.render(<RecentChanges changes={changes.changes} />, document.getElementById('changestarget'))
-}
-
-
-if (typeof window.fetch !== 'undefined') {
-  log('Fetching data...')
-  fetch(HISTORY_ENDPOINT)
-    .then((resp) => resp.json())
-    .then((history) => {
-      mount_history(history)
-    })
-} else {
-  log('No window.fetch.')
-}
+ReactDOM.render(<RecentChanges/>, document.getElementById('changestarget'))
--- scripts/components/RecentChanges.js
+++ scripts/components/RecentChanges.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import Emoji from './Emoji'
 import GuildIcon from './GuildIcon'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const HISTORY_ENDPOINT =
+  window.location.host.endsWith('now.sh') ||
+  'https://api.mousey.app/v3/emoji/blobs+community-blobs/changes'
 
 
 function RenderChangeSet(props) {
@@ -54,18 +59,51 @@ RenderChangeSet.propTypes = {
   changeSet: PropTypes.array.isRequired,
 }
 
-export default function RecentChanges(props) {
-  return (
-    <div>
-      {Object.keys(props.changes).map((item) => {
-        return (
-          <RenderChangeSet changeSet={props.changes[item]} key={item}/>
-        )
-      })}
-    </div>
-  )
-}
+export default class RecentChanges extends Component {
+  constructor(props) {
+    super(props)
 
-RecentChanges.propTypes = {
-  changes: PropTypes.object.isRequired, // TODO
+    this.state = {
+      changes: {},
+    }
+  }
+
+  historyTransform = (historyData) => {
+    let changes = {}
+    for (const change of historyData) {
+      if (['EMOJI_CREATE', 'EMOJI_RENAME', 'EMOJI_UPDATE', 'EMOJI_REMOVE'].includes(change.event)) {
+        const date = change.changed_at = new Date(change.changed_at)
+        const key = [change.guild.id, date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()]
+        changes[key] = (changes[key] ?? [])
+        changes[key].push(change)
+      }
+    }
+    this.setState({ changes: changes })
+  }
+
+  componentDidMount = async () => {
+    const response = await fetch(HISTORY_ENDPOINT)
+    const json = await response.json()
+    this.historyTransform(json)
+  }
+
+  render() {
+    const { changes } = this.state
+
+    if (changes.length === 0) {
+      return (
+        <CircularProgress/>
+      )
+    }
+
+    return (
+      <div>
+        {Object.keys(this.state.changes).map((item) => {
+          return (
+            <RenderChangeSet changeSet={this.state.changes[item]} key={item}/>
+          )
+        })}
+      </div>
+    )
+  }
 }
