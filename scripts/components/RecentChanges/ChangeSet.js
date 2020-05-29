aec033f502763da38cda2c6e236f45231f9822d1
slice <ryaneft@gmail.com>
2020-05-29 09:45:17 -0700

    Update scripts/components/RecentChanges/ChangeSet.js
    Co-authored-by: Lagicrus <Lagicrus@users.noreply.github.com>

--- scripts/components/RecentChanges/ChangeSet.js
+++ scripts/components/RecentChanges/ChangeSet.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import {
