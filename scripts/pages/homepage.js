6e68f225625e610715a3ad239c151ef440f00658
Sam <Lagicrus@users.noreply.github.com>
2020-10-23 12:41:45 +0100

    Move community servers

--- scripts/pages/homepage.js
+++ scripts/pages/homepage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import server1 from 'url:../../assets/server_icons/server1.svg'
-import nitro from 'url:../../assets/discord/nitro.png'
 import OfficialServers from '../components/officialServers'
+import CommunityServers from '../components/communityServers'
 
 class Homepage extends Component {
   render() {
@@ -38,37 +38,7 @@ class Homepage extends Component {
             license.
           </p>
         </section>
-
-        <section id="community-blob-servers">
-          <h2>Community Blob Servers</h2>
-
-          <p>
-            To add your Blob Server to our Community Servers section, join the
-            official
-            <a href="https://1.blobs.gg" target="_blank">Blob Emoji server</a>
-            and message our Blob Mail bot.
-          </p>
-
-          <p>
-            In order to use custom emoji in other Discord Servers you need an
-            active
-            <img
-              className="inline-icon"
-              src={nitro}
-              alt="Discord Nitro icon"
-            />
-            <a
-              href="https://discordapp.com/nitro"
-              target="_blank"
-              className="strong"
-              rel="noopener"
-            >Discord Nitro</a
-            >
-            subscription.
-          </p>
-          {/*React will be mounted into this DOM node*/}
-          <div className="community-guilds-wrapper"/>
-        </section>
+        <CommunityServers/>
       </main>
     )
   }
