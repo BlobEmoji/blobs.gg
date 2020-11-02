f50dad944878b99f4e55cc6678af1174cb11645c
Sam <Lagicrus@users.noreply.github.com>
2020-11-02 17:21:57 +0000

    Simplify changepage to function

--- scripts/pages/changepage.js
+++ scripts/pages/changepage.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import RecentChangesWrapper from '../components/RecentChanges/RecentChanges'
 import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
 
-class Changepage extends Component {
-  render() {
-    return (
-      <Container maxWidth="md">
-        <Typography variant="h4">Global Blob Changelog</Typography>
-        <p>
-          This page tracks the changes of all blobs in any of our partnered
-          servers.
-        </p>
+function ChangePage() {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h4">Global Blob Changelog</Typography>
+      <p>
+        This page tracks the changes of all blobs in any of our partnered
+        servers.
+      </p>
+      <Grid container spacing={3}>
         <RecentChangesWrapper />
-      </Container>
-    )
-  }
+      </Grid>
+    </Container>
+  )
 }
 
-export default Changepage
+export default ChangePage
