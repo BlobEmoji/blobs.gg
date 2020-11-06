c5d1317a25421bea7d4de3c8d99de7dcf95784a5
joaoh1 <joaoh1@users.noreply.github.com>
2020-11-05 23:36:47 -0300

    Fix the margin of the community server text

--- scripts/components/communityServers.js
+++ scripts/components/communityServers.js
@@ -6,14 +6,25 @@ import { shuffleArray } from '../utils'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+const useStyles = makeStyles({
+  boxMargin: {
+    margin: '2rem 0',
+  },
+  h5Margin: {
+    margin: '0 0 1rem 0',
+  },
+})
 
 function CommunityServers(props) {
+  const classes = useStyles()
   let shuffled = props.waiting ? [] : shuffleArray(props.emojis.guilds)
 
   return (
     <>
-      <Box>
-        <Typography variant="h5">Community Blob Servers</Typography>
+      <Box className={classes.boxMargin}>
+        <Typography variant="h5" className={classes.h5Margin}>Community Blob Servers</Typography>
         <Typography variant="body2">
           To add your Blob Server to our Community Servers section, join the
           official <Link href="https://1.blobs.gg" target="_blank" rel="noreferrer">Blob Emoji server</Link> and message
