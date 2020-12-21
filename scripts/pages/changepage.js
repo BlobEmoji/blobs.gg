2173e9a5c974e9ea531955f1369bc43e40d28d05
joaoh1 <joaoh1@users.noreply.github.com>
2020-12-20 22:16:56 -0300

    Fix spacing on the changelog page

--- scripts/pages/changepage.js
+++ scripts/pages/changepage.js
@@ -3,12 +3,20 @@ import Typography from '@material-ui/core/Typography'
 import RecentChangesWrapper from '../components/RecentChanges/RecentChanges'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+const useStyles = makeStyles({
+  description: {
+    margin: '1rem 0',
+  },
+})
 
 function ChangePage() {
+  const classes = useStyles()
   return (
     <Container maxWidth="md">
       <Typography variant="h4">Global Blob Changelog</Typography>
-      <Typography variant="body1">
+      <Typography className={classes.description} variant="body1">
         This page tracks the changes of all blobs in any of our partnered
         servers.
       </Typography>
