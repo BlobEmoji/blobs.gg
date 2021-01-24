70c06f9265efffc7c118b97de5f833d889097a8b
Lagicrus <Lagicrus@users.noreply.github.com>
2021-01-24 18:41:09 +0000

    [Global] Swap out how fonts are loaded (#42)
    * [Global] Swap out how fonts are loaded
    
    * Reorder packages
    Yarn keeps doing this thing
    
    Co-authored-by: joaoh1 <joaoh1@users.noreply.github.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import '@fontsource/roboto/latin-400.css'
+import '@fontsource/roboto/latin-700.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
