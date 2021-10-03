c052ef683c9eb7419f71f1f2cf34b1e9c6d50382
Lagicrus <Lagicrus@users.noreply.github.com>
2021-10-03 19:05:08 +0100

    [site] Add drawfest support (#112)
    * [site] Add drawfest support
    
    * Clean up everything, remove links to the main site
    
    * Prettified Code!
    
    * [site] Revert Changes
    
    * Compromise with the changes
    One last change that I want to keep though, but this should be enough
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    Co-authored-by: EnnuiL <EnnuiL@users.noreply.github.com>

---
+++ scripts/components/Drawfest/SkeletonSubmissions.js
@@ -0,0 +1,60 @@
+import React, { memo } from "react";
+import Skeleton from "@material-ui/core/Skeleton";
+import TableBody from "@material-ui/core/TableBody";
+import { arrayRange } from "../../utils";
+import { TableCell, TableRow } from "@material-ui/core";
+
+function SkeletonApprovedSubmissions() {
+  const amounts = arrayRange(3, 8);
+  const [length] = React.useState(
+    amounts[Math.floor(Math.random() * amounts.length)]
+  );
+  return (
+    <TableCell width={64 * 11}>
+      {Array.from({ length }, (_, index) => (
+        <Skeleton
+          variant="circular"
+          width={32}
+          height={32}
+          key={index}
+          sx={{ display: "inline-block", marginRight: 1 }}
+        />
+      ))}
+    </TableCell>
+  );
+}
+
+function SkeletonNames() {
+  const amounts = [50, 70, 90, 110, 130];
+  const [length] = React.useState(
+    amounts[Math.floor(Math.random() * amounts.length)]
+  );
+  return (
+    <TableCell>
+      <Skeleton variant="rectangular" width={length} height={20} />
+    </TableCell>
+  );
+}
+
+const SkeletonSubmissions = memo(function SkeletonSubmissions() {
+  const amounts = arrayRange(8, 14);
+  const [length] = React.useState(
+    amounts[Math.floor(Math.random() * amounts.length)]
+  );
+
+  const skeletons = Array.from({ length }, (_, index) => {
+    return (
+      <TableRow key={index}>
+        <TableCell>
+          <Skeleton variant="circular" width={64} height={64} />
+        </TableCell>
+        <SkeletonNames />
+        <SkeletonApprovedSubmissions />
+      </TableRow>
+    );
+  });
+
+  return <TableBody>{skeletons}</TableBody>;
+});
+
+export default SkeletonSubmissions;
